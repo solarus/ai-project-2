0e1477140d5e4f5784a9813ee641018ec8b97286
David Spångberg <solarus@tunna.org>
2013-05-08 17:07:06 +0200

    add capitalize function to Utils

--- www/planner-haskell/src/Utils.hs
+++ www/planner-haskell/src/Utils.hs
@@ -1,8 +1,13 @@
 module Utils where
 
+import Data.Char
+
 nGrams :: Int -> [a] -> [[a]]
 nGrams n = takeWhile ((==n) . length) . map (take n) . iterate (drop 1)
 
 listToPair :: [a] -> (a, a)
 listToPair [a,b] = (a,b)
 listToPair _     = error "Utils.listToPair: Need to be exactly two elements in the list"
+
+capitalize :: String -> String
+capitalize (c:cs) = toUpper c : cs

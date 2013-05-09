61ed952afe3fab2b89485850536556126ac6167c
David Spångberg <solarus@tunna.org>
2013-05-09 14:37:51 +0200

    add shell script wrapper for ff

---
+++ bin/ff-wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+domain="../planner-haskell/resources/shrdlu-dom.pddl"
+problem_file=$1
+
+ff_out=$(ff -o $domain -f $problem_file)
+
+actions=$(echo -e "$ff_out" | egrep "((PICK|DROP)-(IN|ON))")
+
+# Todo error handling
+echo "$actions" | cut -b 12- | sed -re 's/^(PICK|DROP).*F(\w*)$/\L\1 \2/'

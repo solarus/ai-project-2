9c4f8d9f323bba03a3cffac85c46596897c954cb
Bartolomeus Jankowski <brtk666@gmail.com>
2013-05-15 17:37:47 +0200

    Fix for OS plus gitignore

--- bin/ff-wrapper.sh
+++ bin/ff-wrapper.sh
@@ -3,6 +3,8 @@
 domain="../planner-haskell/resources/shrdlu-dom.pddl"
 problem_file=$1
 
+PATH=$PATH:/usr/local/bin/
+
 ff_out=$(../bin/ff -o $domain -f $problem_file)
 
 case $ff_out in
@@ -15,4 +17,9 @@ esac
 actions=$(echo -e "$ff_out" | egrep "((PICK|DROP)-(IN|ON))")
 
 # Todo error handling
-echo "$actions" | cut -b 12- | sed -re 's/^(PICK|DROP).*F(\w*)$/\L\1 \2/'
+# fix for BSD, use 'gsed' if present...
+if hash gsed 2>/dev/null; then
+  echo "$actions" | cut -b 12- | gsed -re 's/^(PICK|DROP).*F(\w*)$/\L\1 \2/'
+else
+  echo "$actions" | cut -b 12- | sed -re 's/^(PICK|DROP).*F(\w*)$/\L\1 \2/'
+fi
